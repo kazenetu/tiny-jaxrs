2c9e91d9ae89bb48703775f8def55651f856eec3
kazenetu <rakudafactory@gmail.com>
2017-03-04 16:12:01 +0900

    WebApiのルートパスをappからapiに変更

--- web/src/main/java/web/ServerApplication.java
+++ web/src/main/java/web/ServerApplication.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("app")
+@ApplicationPath("api")
 public class ServerApplication extends ResourceConfig {
 
     public ServerApplication() {
