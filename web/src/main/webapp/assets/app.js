12661b76b1abca3865f1419f57426cf58cfc46e3
kazenetu <rakudafactory@gmail.com>
2017-03-05 16:28:20 +0900

    ファイルダウンロード時の401エラー発生でログイン画面に戻す処理を実装

--- web/src/main/webapp/assets/app.js
+++ web/src/main/webapp/assets/app.js
@@ -1,20 +1,15 @@
-var myApp = angular.module('App', 
-[
-    'ngResource'
-    ,'ngRoute'
-    ,'headerController'
-    ,'loginController'
+var myApp = angular.module('App', [ 'ngResource', 'ngRoute', 'headerController', 'loginController', 'searchSample' ]);
 
-    ,'searchSample'
+myApp.config([ '$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl : 'view/login.html'
+    }).when('/main', {
+        templateUrl : 'view/main.html',
+    }).otherwise({
+        redirectTo : '/'
+    });
+
+    // ルートURL退避
+    sessionStorage.setItem("rootURL",location.protocol + '//' + location.host + '/web/');
+}
 ]);
-
-myApp.config(['$routeProvider',function($routeProvider){
-  $routeProvider
-    .when('/', {
-        templateUrl: 'view/login.html'
-    })
-    .when('/main', {
-        templateUrl: 'view/main.html',
-    })
-    .otherwise({ redirectTo: '/'});
-}]);
