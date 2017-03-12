81a558428227a6db0c5bf0813f7c1c6706e273f0
kazenetu <rakudafactory@gmail.com>
2017-03-13 08:04:38 +0900

    検索画面から取得したキー情報をもとにユーザーデータの取得と表示を実装

--- web/src/main/webapp/assets/controller/master/userEdit.js
+++ web/src/main/webapp/assets/controller/master/userEdit.js
@@ -9,7 +9,18 @@ function UserEdit($location, webApiService, userService,storageService) {
     ctrl.password = "";
 
     ctrl.init = function(){
+        // 検索画面から取得したキー情報を設定
         ctrl.userId = storageService.getValue(storageService.KEY_EDIT_DATA);
+
+        // ユーザーデータ取得
+        webApiService.query('api/user/page?userId=:id&page=:pageIndex&searchUserId=:searchUserId', {
+            id : userService.getId(),
+            pageIndex : 0,
+            searchUserId : ctrl.userId
+        }, function(response) {
+            ctrl.userName = response[0].name;
+            ctrl.password = response[0].password;
+        });
     }
 
     ctrl.update = function(){
@@ -17,6 +28,10 @@ function UserEdit($location, webApiService, userService,storageService) {
     }
 
     ctrl.cancel = function(){
+        console.log("ctrl.userId:"+ctrl.userId);
+        console.log("ctrl.userName:"+ctrl.userName);
+        console.log("ctrl.password:"+ctrl.password);
+
         $location.path('/main');
     }
 
