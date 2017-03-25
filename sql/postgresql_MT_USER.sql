04fc853e1680a4b7bbf10751b5aa85ecc598ecbd
kazenetu <rakudafactory@gmail.com>
2017-03-25 20:12:24 +0900

    日付系カラムを追加

--- sql/postgresql_MT_USER.sql
+++ sql/postgresql_MT_USER.sql
@@ -3,5 +3,8 @@ create table MT_USER (
   USER_ID VARCHAR(30)
   , NAME VARCHAR(30)
   , PASSWORD VARCHAR(30)
+  , DATE_DATA date
+  , TIME_DATA time without time zone
+  , TS_DATA timestamp without time zone
   , constraint MT_USER_PKC primary key (USER_ID)
 ) ;
--- sql/sqlite_MT_USER.sql
+++ sql/sqlite_MT_USER.sql
@@ -3,5 +3,8 @@ create table MT_USER (
   USER_ID NVARCHAR
   , NAME NVARCHAR
   , PASSWORD NVARCHAR
+  , DATE_DATA date
+  , TIME_DATA time without time zone
+  , TS_DATA timestamp without time zone
   , primary key (USER_ID)
 );
