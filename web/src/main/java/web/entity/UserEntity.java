96149c16e91d9d2e0f53d4394646837d8fb2c5ae
kazenetu <rakudafactory@gmail.com>
2017-03-25 20:28:57 +0900

    日付系フィールド追加

--- web/src/main/java/web/entity/UserEntity.java
+++ web/src/main/java/web/entity/UserEntity.java
@@ -1,10 +1,18 @@
 package web.entity;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+
 public class UserEntity {
     private String id;
     private String name;
     private String password;
     private int age;
+    private String date;
+    private String time;
+    private String ts;
 
 
     public UserEntity() {
@@ -48,4 +56,57 @@ public class UserEntity {
     public void setAge(int age) {
         this.age = age;
     }
+
+    /**
+     * @return date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * @param date セットする date
+     */
+    public void setDate(Date date) {
+        this.date = null;
+        if(date != null){
+            DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+            this.date = date.toLocalDate().format(f);
+        }
+    }
+
+    /**
+     * @return time
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * @param time セットする time
+     */
+    public void setTime(Time time) {
+        if(time != null){
+            this.time = time.toString();
+        }
+    }
+
+    /**
+     * @return ts
+     */
+    public String getTs() {
+        return ts;
+    }
+
+    /**
+     * @param ts セットする ts
+     */
+    public void setTs(Timestamp ts) {
+        this.ts = null;
+        if(ts != null){
+            DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            this.ts =  ts.toLocalDateTime().format(f);
+        }
+    }
+
 }
