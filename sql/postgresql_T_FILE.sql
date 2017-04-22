0f3de331b9c1d307d09691d17b005ba5e1998aa4
kazenetu <rakudafactory@gmail.com>
2017-04-22 14:00:20 +0900

    T_FILEにタグ情報(TAG)を追加

--- sql/postgresql_T_FILE.sql
+++ sql/postgresql_T_FILE.sql
@@ -3,6 +3,7 @@ create table T_FILE (
   ID integer not null
   , IMAGE_DATA character varying(1000000)
   , FILE_NAME character varying(50)
+  , TAG character varying(8)
   , constraint T_FILE_PKC primary key (ID)
 ) ;
 
@@ -10,4 +11,5 @@ comment on table T_FILE is 'ファイルテーブル';
 comment on column T_FILE.ID is 'ID';
 comment on column T_FILE.IMAGE_DATA is 'BASE64ファイルデータ';
 comment on column T_FILE.FILE_NAME is 'ファイル名';
+comment on column T_FILE.TAG is 'タグ情報';
 
--- sql/sqlite_MT_T_FILE.sql
+++ sql/sqlite_MT_T_FILE.sql
@@ -3,6 +3,7 @@ create table T_FILE (
   ID integer not null
   , IMAGE_DATA character varying(1000000)
   , FILE_NAME character varying(50)
+  , TAG character varying(8)
   , constraint T_FILE_PKC primary key (ID)
 ) ;
 
