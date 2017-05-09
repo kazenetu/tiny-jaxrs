c36477a16cfdf96aee51ad1a7524f364b078eedb
kazenetu <rakudafactory@gmail.com>
2017-05-09 19:48:20 +0900

    時刻用文字列の長さを4桁から6桁に変更

--- sql/postgresql_T_FILE.sql
+++ sql/postgresql_T_FILE.sql
@@ -4,7 +4,7 @@ create table T_FILE (
   , IMAGE_DATA character varying(1000000)
   , FILE_NAME character varying(50)
   , TAG character varying(8)
-  , TIME_DATA character varying(4)
+  , TIME_DATA character varying(6)
   , constraint T_FILE_PKC primary key (ID)
 ) ;
 
