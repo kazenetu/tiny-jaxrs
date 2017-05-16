09c5118552341e24f9d194baab375b9dd76bea50
kazenetu <rakudafactory@gmail.com>
2017-05-16 20:16:46 +0900

    コメントミスを修正

--- web/src/main/java/web/common/messagebody/CsvWriter.java
+++ web/src/main/java/web/common/messagebody/CsvWriter.java
@@ -59,7 +59,7 @@ public class CsvWriter<E> implements MessageBodyWriter<CsvEntity<E>> {
         try (PrintWriter writer = new PrintWriter(new OutputStreamWriter(entityStream, Charset.forName("Windows-31J")));
                 CSVPrinter printer = new CSVPrinter(writer, CSVFormat.RFC4180.withQuoteMode(QuoteMode.NON_NUMERIC));) {
 
-            // ヘッダカラム指定がない場合は終了
+            // ヘッダカラム指定がある場合は終了
             if (csvEntity.isExitstColumnHeaders()) {
                 for(String columnHeader : csvEntity.getColumnHeaders()){
                     printer.print(columnHeader);
