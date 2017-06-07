da7c5cce3b9bb2f803b837f164df1028ad644149
kazenetu <rakudafactory@gmail.com>
2017-06-07 20:03:34 +0900

    メッセージ取得ユーティリティの不要なメソッドを削除

--- web/src/main/java/web/common/util/MessageUtil.java
+++ web/src/main/java/web/common/util/MessageUtil.java
@@ -2,7 +2,6 @@ package web.common.util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,39 +32,12 @@ public class MessageUtil {
     private class MessageModel extends Model {
 
         /**
-         * メッセージをDBから取得する
-         * @return メッセージ一覧
-         * @throws Exception
-         */
-        public Map<String,String> selectAll() throws Exception{
-            String sql = "select message_kind,message_id,message from MT_MSG;";
-
-            Map<String,String> entities = new HashMap<>();
-
-            try {
-                List<Map<String,Object>> result = db.query(sql, new ArrayList<>());
-                if (!result.isEmpty()) {
-                    result.forEach(row->{
-                        String key = String.format("%s%04d", (String)getColumnValue(row,"message_kind"), (int)getColumnValue(row,"message_id"));
-                        entities.put(key, (String)getColumnValue(row,"message"));
-                    });
-                }
-            } catch (Exception e) {
-                throw new Exception(e);
-            }
-
-            return entities;
-        }
-
-        /**
          * メッセージをリソースファイルから取得
          * @return メッセージ一覧
          * @throws Exception
          */
         public Map<String,String> getJson() throws Exception{
-            // JSONファイルは下記SQLでメッセージマスタから作成
-            // select '{"key":"' || trim(message_kind) || trim(to_char(message_id,'0000')) || '","value":"' || message || '"},' as json from mt_msg;
-
+            // JSONファイルはmessage.jsonから取得
             String filePath = this.getClass().getClassLoader().getResource("message.json").getPath();
             StringBuilder builder = new StringBuilder();
 
@@ -100,12 +72,8 @@ public class MessageUtil {
     private MessageUtil(){
         // メッセージを取得する
         try (MessageModel model = new MessageModel()) {
-            // DBから取得
-           //messages = model.selectAll();
-
             // jsonファイルから取得
             messages = model.getJson();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
